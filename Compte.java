4b57e14049f3f8ac968bf736ba380edd2d554a46
zjulien <zinet.julien@hotmail.com>
2019-08-18 14:11:16 +0200

    exercice classe compte bancaire

---
+++ Compte.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicejava;
+
+/**
+ *
+ * @author stagiaire acs
+ */
+public class Compte {
+    
+    public Double solde;
+    public Compte(Double s){
+        this.solde=s;
+    }
+    public void deposer(Double d){
+        this.solde+=d;
+    }
+    public void retirer(Double r){
+        this.solde-=r;
+    }
+    
+    public void afficher(){
+        System.out.println("votre solde est " + this.solde + "Euro Sauf erreur ou omission");
+    }
+    
+}
